0ba02e614fc6f841d5e120566a23eb33eccdb3d7
JavadEsmati <Javad.esmatib@Gmail.com>
2025-04-06 21:21:58 +0200

    add new style for pizza component

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,14 +1,10 @@
 const Pizza = (props) => {
   return (
     <div className="flex flex-col items-center justify-center gap-2">
-      <h3 className="text-secondary mb-2 text-2xl">{props.name}</h3>
-      <p className="mb-1">{props.description}</p>
-      <img
-        src={props.image}
-        alt={props.name}
-        className="border-border shadow-card max-w-[200px] rounded-xl border"
-      />
-      <p className="text-lg font-bold">{props.price}</p>
+      <h3 className="pizza-name">{props.name}</h3>
+      <p className="pizza-description">{props.description}</p>
+      <img src={props.image} alt={props.name} className="pizza-image" />
+      <p className="pizza-price">{props.price}</p>
     </div>
   );
 };
